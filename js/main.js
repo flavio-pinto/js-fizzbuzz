ab0bd31407cfd1ca5c26713c0336b027798ed85b
Flavio Pinto <flaviopnt@hotmail.it>
2020-04-01 16:25:26 +0200

    JS Program - OK

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@
  * per i multipli di 5 stampi Buzz.
  * per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
  */
+
+
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz')
+    } else {
+        console.log(i);
+    }
+}
